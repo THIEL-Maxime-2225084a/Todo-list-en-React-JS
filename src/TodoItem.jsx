6052799d2a256a0fae592c53926dfa91000a1920
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2024-03-28 02:09:17 +0100

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,20 +1,51 @@
-import React, { Component } from 'react'
-import './App.css'
-import { MdCheckBoxOutlineBlank, MdCalendarMonth, MdEdit, MdDelete } from 'react-icons/md'
+import React, { Component } from 'react';
+import './App.css';
+import { MdCheckBox, MdCheckBoxOutlineBlank, MdCalendarMonth, MdEdit, MdDelete } from 'react-icons/md';
 
 export default class TodoItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      editing: false,
+      editedTitle: this.props.title,
+    };
   }
-  
+
+  editButtonClick = () => {
+    this.setState({ 
+      editing: true 
+    });
+  }
+
+  inputChange = (evt) => {
+    this.setState({ 
+      editedTitle: evt.target.value 
+    });
+  }
+
+  inputKeyDown = (evt) => {
+    if (evt.key === 'Enter') {
+      this.editTitle();
+    }
+  }
+
+  editTitle = () => {
+    if (this.state.editedTitle) {
+      onEdit(this.props.id, this.state.editedTitle);
+      this.setState({ editing: false });
+    }
+  }
+
   render() {
-    return(
+    const { id, title, selected, onCheck, onDelete, onEdit } = this.props;
+    const { editing, editedTitle } = this.state;
+    return (
       <div className="item-container">
-        <button><MdCheckBoxOutlineBlank/></button>
-        {this.props.title}
+        {selected ? (<button onClick={() => onCheck(id)}><MdCheckBox/></button>) : (<button onClick={() => onCheck(id)}><MdCheckBoxOutlineBlank/></button>)}
+        {editing ? (<input type="text" value={editedTitle} onChange={this.inputChange} onKeyDown={this.inputKeyDown}/>) : (title)}
         <button><MdCalendarMonth/></button>
-        <button><MdEdit/></button> 
-        <button onClick={() => this.props.onDelete(this.props.id)}><MdDelete/></button>
+        {editing ? (<button onClick={this.editTitle}><MdEdit/></button>) : (<button onClick={this.editButtonClick}><MdEdit/></button>)}
+        <button onClick={() => onDelete(id)}><MdDelete/></button>
         <br/>
       </div>
     );
